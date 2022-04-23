c2577a0b10457ad9dd63718418c471be4f05f3af
DylanCarrGMIT <61824332+DylanCarrGMIT@users.noreply.github.com>
2022-04-23 17:17:23 +0100

    added some local storage functionality
    text input now gets added to arrays in local storage

--- src/App.js
+++ src/App.js
@@ -3,33 +3,53 @@ import { Text, View } from 'react-native';
 import { Button, TextInput } from 'react-native-web';
 
 const App = () => {
+   
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#7f7f7f" }}>
       <Text>
        <h3><b>Budget Buddy</b></h3>
       
       </Text>
-      <TextInput
-      style={{
-          height: 40,
-          borderColor: 'orange',
-          borderWidth: 5
-        }}
-        placeholder="Enter amount spent here!"/>
-         <TextInput
-      style={{
-          height: 40,
-          borderColor: 'orange',
-          borderWidth: 5,
-        }}
-        placeholder="Enter category here!"/>
-        <Button
+      <input id="amount"
+        style={{
+            height: 40,
+            borderColor: 'orange',
+            borderWidth: 5
+          }}
+          placeholder="Enter amount spent here!"/>
+      <input id = "category"
+          style={{
+              height: 40,
+              borderColor: 'orange',
+              borderWidth: 5,
+            }}
+            placeholder="Enter category here!"/>
+      <Button
           onPress={() => {
-            alert('You tapped the button!');
+            addToLog();
           }}
           title="Add to log" color="orange"/>
     </View>
   );
 }
+function addToLog()
+{
+  var amount = [];
+  var category = [];
+  if("amount" in localStorage)
+  {
+   amount = JSON.parse(localStorage.getItem("amount"));
+  }
+  if("category" in localStorage)
+  {
+   category = JSON.parse(localStorage.getItem("category"));
+  }
 
+  alert("Added to log!");
+  
+  amount.push(document.getElementById("amount").value);
+  window.localStorage.setItem("amount",JSON.stringify(amount));
+  category.push(document.getElementById("category").value);
+  window.localStorage.setItem("category",JSON.stringify(category));
+}
 export default App;
